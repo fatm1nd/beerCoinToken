314ec56b27d2c6ccd93ab8652ee0576d2c0dbade
fatm1nd <semeritskiy@yandex.ru>
2023-06-30 21:03:09 +0300

    Add "getBalances" and "getUsers" features.

--- beerCoinContract.sol
+++ beerCoinContract.sol
@@ -5,12 +5,18 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BeerCoin is ERC20 {
+    // 000000000000000000 - 18 zeros
+    // 
+    mapping(address => uint256) private __balances;
+    address[] private addressesList;
 
     address barmen = 0xD8C7978Be2A06F5752cB727fB3B7831B70bF394d;
     uint256 barmenFee = 50 * (10 ** decimals());
 
     constructor(uint256 initialSupply) ERC20("BeerCoin", "PNT") {
         _mint(msg.sender, initialSupply * (10 ** decimals()));
+        returnAddressID(barmen);
+        returnAddressID(msg.sender);
     }
 
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
@@ -18,6 +24,7 @@ contract BeerCoin is ERC20 {
         require(balanceOf(owner) >= (barmenFee + amount), "Your account does not have enough money (fee to barmen + amount)");
         _transfer(owner, to, amount);
         _transfer(owner, barmen, barmenFee);
+        copyTransaction(owner,to);
         return true;
     }
 
@@ -28,6 +35,7 @@ contract BeerCoin is ERC20 {
         _spendAllowance(from, spender, amount);
         _transfer(from, to, amount);
         _transfer(from, barmen, barmenFee);
+        copyTransaction(from,to);
         return true;
     }
 
@@ -40,4 +48,43 @@ contract BeerCoin is ERC20 {
             }
         }
     }
+
+    function getAll() public view returns ( address[] memory, uint256[] memory){
+        
+        uint256[] memory users = new uint256[](addressesList.length);
+        address userAddress;
+        for (uint256 i = 0; i < addressesList.length; i++) 
+        {
+            userAddress = addressesList[i];
+            users[i] = balanceOf(userAddress);  
+        }
+        return (addressesList,users);
+    }
+
+    function getUsers() public view returns (address[] memory){
+        return addressesList;
+    }
+
+    function returnAddressID(address user) internal returns (uint256){
+        for (uint256 i = 0; i < addressesList.length; i++) 
+        {
+            if (addressesList[i] == user){
+                return i;
+            }   
+        }
+        addressesList.push(user);
+        __balances[user] = 0;
+        return addressesList.length - 1;
+    }
+
+    function copyTransaction(address from, address to) internal virtual {
+        returnAddressID(from);
+        returnAddressID(to);
+        unchecked {
+            __balances[from] = balanceOf(from);
+            __balances[to] += balanceOf(to);
+            __balances[barmen] += balanceOf(barmen);
+        }
+    }
+
 }
